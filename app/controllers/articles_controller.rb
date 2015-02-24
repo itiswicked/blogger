6aec0ee3191a997ef91b6be3048046afa86df182
Nathan Bourke <nathanrbourke@gmail.com>
2015-02-23 20:50:55 -0500

    Add flash notice for Create and Destroy, change root to articles#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,8 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "Article '#{@article.title}' created!"
     redirect_to article_path(@article)
   end
 
@@ -34,6 +36,8 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' deleted!"
     redirect_to articles_path
   end
 end
